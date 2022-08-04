0116646949d7d3d767c260c70460fc7a88360d60
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2022-08-04 22:46:56 +0300

    Add DISPLAY_NAME projection when doing ContentResolver to get filename

--- app/src/main/java/com/viliussutkus89/documenter/utils/UriUtils.kt
+++ app/src/main/java/com/viliussutkus89/documenter/utils/UriUtils.kt
@@ -31,7 +31,7 @@ fun Uri.getFilename(contentResolver: ContentResolver): String? {
     if ("file" == scheme) {
         return path?.let { File(it).name }
     } else {
-        contentResolver.query(this, null, null, null, null).use { crs ->
+        contentResolver.query(this, arrayOf(OpenableColumns.DISPLAY_NAME), null, null, null).use { crs ->
             crs?.let {
                 crs.moveToFirst()
                 val nameIndex = crs.getColumnIndex(OpenableColumns.DISPLAY_NAME)
