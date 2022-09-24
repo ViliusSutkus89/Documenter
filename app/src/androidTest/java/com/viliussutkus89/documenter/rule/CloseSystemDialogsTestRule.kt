33c73391be8a9d0cdb955dfd6c836636c3a4b678
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2022-09-24 08:14:37 +0300

    [Tests] Update CloseSystemDialogsTestRule to use TestRule instead of ExternalResource
    Cancel dialog watcher in after()

--- app/src/androidTest/java/com/viliussutkus89/documenter/rule/CloseSystemDialogsTestRule.kt
+++ app/src/androidTest/java/com/viliussutkus89/documenter/rule/CloseSystemDialogsTestRule.kt
@@ -1,21 +1,34 @@
+/*
+ * CloseSystemDialogsTestRule.kt
+ *
+ * Copyright (C) 2022 https://www.ViliusSutkus89.com/documenter/
+ *
+ * I am Speed is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.viliussutkus89.documenter.rule
 
 import android.content.Intent
 import android.os.Build
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.uiautomator.UiDevice
-import androidx.test.uiautomator.UiSelector
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.yield
-import org.junit.rules.TestRule
-import org.junit.runner.Description
-import org.junit.runners.model.Statement
+import kotlinx.coroutines.*
+import org.junit.rules.ExternalResource
 
 
-class CloseSystemDialogsTestRule: TestRule {
-    override fun apply(base: Statement, description: Description?): Statement {
+class CloseSystemDialogsTestRule: ExternalResource() {
+    var dialogClicker: Job? = null
+
+    override fun before() {
         // ACTION_CLOSE_SYSTEM_DIALOGS is perfectly fine in tests
         @Suppress("DEPRECATION")
         InstrumentationRegistry.getInstrumentation().context
@@ -23,19 +36,18 @@ class CloseSystemDialogsTestRule: TestRule {
 
         // Watch for ANRs and click wait
         if (Build.VERSION.SDK_INT >= 18) {
-            @OptIn(DelicateCoroutinesApi::class)
-            GlobalScope.launch {
-                val anrSelector = UiSelector()
+            dialogClicker = CoroutineScope(Dispatchers.Default).launch {
+                val anrSelector = androidx.test.uiautomator.UiSelector()
                     .packageName("android")
                     .textContains("isn't responding")
 
-                val waitButtonSelector = UiSelector()
+                val waitButtonSelector = androidx.test.uiautomator.UiSelector()
                     .packageName("android")
                     .className("android.widget.Button")
                     .textContains("Wait")
 
-                val uiDevice: UiDevice =
-                    UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+                val uiDevice: androidx.test.uiautomator.UiDevice =
+                    androidx.test.uiautomator.UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
                 while (true) {
                     yield()
                     if (uiDevice.findObject(anrSelector).exists()) {
@@ -47,7 +59,9 @@ class CloseSystemDialogsTestRule: TestRule {
                 }
             }
         }
+    }
 
-        return base
+    override fun after() {
+        dialogClicker?.cancel()
     }
 }
