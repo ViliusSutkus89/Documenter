5938cb5ab4c1d172e2c96095e75948b0b024f8bb
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-01-04 04:11:10 +0200

    Cosmetic change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,9 +41,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    kotlinOptions.jvmTarget = "1.8"
 
     splits.abi {
         isEnable = true
