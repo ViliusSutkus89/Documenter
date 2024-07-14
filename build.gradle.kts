abce622eb026d202aae4d03a65045517ae9030a6
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-07-14 17:33:11 +0300

    Upgrade Kotlin to 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.5.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.mikepenz.aboutlibraries.plugin") version "10.10.0" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.6" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.16" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.23" apply false
 }
