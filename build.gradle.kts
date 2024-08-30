a9d3863110c5c2d39c8199ea676a1d60bac2335e
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-08-31 02:46:55 +0300

    Upgrade Kotlin to 2.0.20, ksp to 2.0.20-1.0.24

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.5.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
     id("com.mikepenz.aboutlibraries.plugin") version "10.10.0" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.23" apply false
 }
