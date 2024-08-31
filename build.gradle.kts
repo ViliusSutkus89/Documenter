485b011fae5b15e653215ad789a63a5eecabf904
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-08-31 03:04:50 +0300

    Downgrade Kotlin to 1.9.25, because KSP doesn't support 2.0 yet

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
+    // w: Kapt currently doesn't support language version 2.0+. Falling back to 1.9.
+    //noinspection GradleDependency
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    //noinspection GradleDependency
+    id("com.google.devtools.ksp") version "1.9.25-1.0.20" apply false
     id("com.mikepenz.aboutlibraries.plugin") version "10.10.0" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
 }
